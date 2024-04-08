5d78c190cb78a83572772234a0f93084f2b7dda7
Yan <yanocher@gmail.com>
2024-04-08 16:30:05 +0300

    Создал класс Formatter отдельным файлом

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.MissingResourceException;
 import java.util.Scanner;
 
 public class Main {
@@ -11,8 +9,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("На скольких человек необходимо разделить счёт?");
 
+        int man;
         while (true) {
-            int man = scanner.nextInt();
+            man = scanner.nextInt();
             if (man < 1) {
                 System.out.println("Это некорректное значение");
                 System.out.println("На скольких человек необходимо разделить счёт?");
@@ -32,7 +31,7 @@ public class Main {
             System.out.println("Товар успешно добавлен");
             System.out.println("Если хотите добавить ещё один товар введите 'Да', если нет, введите 'Завершить'");
             String command = scanner.next();
-            if (!command.equals("Завершить")) {
+            if (!command.equalsIgnoreCase("Завершить")) {
                 System.out.println("Введите название и стоимость товара");
             } else {
                 System.out.println("Добавленные товары:");
@@ -40,11 +39,43 @@ public class Main {
             }
         }
 
-        //Map.Entry<String, Double> entry = mapIterator.next();
         for (Map.Entry<String, Double> entry : productMap.entrySet()) {
             String product = entry.getKey();
             double price = entry.getValue();
             System.out.println(String.format("%s %.2f рублей", product, price));
         }
+
+        double sum = 0;
+        for (double bill : productMap.values()) {
+            sum += bill;
+        }
+        System.out.println(String.format("Итого: %.2f рублей", sum));
+        double separateBill = sum / man;
+        System.out.println(String.format("Каждый человек должен заплатить: %.2f рублей", separateBill));
+    }
+
+}
+class Formatter {
+    static double number = 211.56;
+    public static void main(String[] args) {
+
+        double result = Math.floor(number);
+        double ld1 = result % 10;
+        double ld2 = result % 100;
+        System.out.println(number);
+
+        if (ld1 == 1 & ld2 != 11) {
+            String rub = "рубль";
+            System.out.println(rub);
+        } else if (ld1 == 2 & ld2 != 12 | ld1 == 3 & ld2 != 13 | ld1 == 4 & ld2 != 14) {
+            String rub = "рубля";
+            System.out.println(rub);
+        } else {
+            String rub = "рублей";
+            System.out.println(rub);
+        }
     }
 }
+
+
+
